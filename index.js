980c90acc9037dc30297b4fc39b2d76a78fcc7f3
ashwini2704 <ashwini.titarmare96@gmail.com>
2023-12-10 17:09:32 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const {connection} = require("./db");
 const { userRouter } = require("./routes/user.route");
 const { noteRouter } = require("./routes/notes.route");
-
+const cors = require("cors")
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use("/users",userRouter);
 app.use("/notes",noteRouter)
 
